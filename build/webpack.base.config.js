ad23aa561d94e03496cc3c04ea7f0c4f44d07491
Carlos <carlosacorti@hotmail.com>
2021-01-04 12:25:37 -0300

    serving vue via webpack

---
+++ build/webpack.base.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "../", "src", "client-entry.js"),
+  },
+
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.js",
+    },
+  },
+
+  output: {
+    path: path.resolve(__dirname, "../", "public", "assets", "js"),
+    publicPath: "/",
+    filename: "[name].js",
+  },
+
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(sass|scss|css)$/,
+  //       // use: [MiniCssExtraxtPlugin.loader, 'css-loader', 'sass-loader'], // in case we use sass
+  //       use: [MiniCssExtraxtPlugin.loader, "css-loader"],
+  //     },
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       loader: "babel-loader",
+  //       options: {
+  //         plugins: ["@babel/transform-runtime"],
+  //       },
+  //     },
+  //   ],
+  // },
+
+  // plugins: [
+  //   // for assets.json list on public folder
+  //   new AssetsPlugin({
+  //     filename: "assets.json",
+  //     path: path.join(__dirname, "../", "public", "build"),
+  //   }),
+  //   // to clean public/build folder each time webpack outputs files
+  //   new CleanWebpackPlugin(),
+  //   new MiniCssExtraxtPlugin({
+  //     filename: "css_[name].[contentHash].css",
+  //   }),
+  // ],
+
+  // optimization: {
+  //   splitChunks: {
+  //     cacheGroups: {
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         chunks: "initial",
+  //         name: "vendor",
+  //         enforce: true,
+  //       },
+  //     },
+  //   },
+  //   minimizer: [new OptimizeCssAssetsWebpackPlugin(), new TerserPlugin()],
+  // },
+};
